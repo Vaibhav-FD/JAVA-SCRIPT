984e0e067143430b3faee08ecc1aabf770a0f9b0
Vaibhav-FD <Vaibhav91201@gmail.com>
2024-03-18 18:42:34 +0530

    JS

--- New Java Script/BASICS/03_JS_Variables_And_DataTypes.js
+++ New Java Script/BASICS/03_JS_Variables_And_DataTypes.js
@@ -88,3 +88,70 @@ const thirdNumber = 27;  // DECLARING AND ASSIGN WITH const
 console.log(`%cActually We Can Also Declare Variable Using var Keyword BUT DO NOT USE var TO DECLARE AND CREATE VARIABLES.`,`color:red;`);
 
 console.log(`%cIF WE DO NOT USE ANY KEY WORD WE CAN ALSO CREATE VARIABLES BUT DO NOT DO THIS.`,`color:red;`);
+
+
+
+
+
+
+console.log(`%cDataTypes`,`color: lime; text-decoration: underline 2px dashed blueviolet; text-underline-offset: 2px; margin-block: 1.29rem; font-size: 1.29rem;`);
+
+
+console.log(`%cDataTypes Are Units Which Will Decide What Kind Or Type Of Data Will Be Stored In Variables.`,`color: #ff9974;`);
+
+console.log(`\nThere Are Mainly Two Types Of Data Types Primitive And NoN Primitive.\n\n`);
+
+console.log(`%cPrimitive Data Types Has 7 Data Types : ---NNUBBS--- %c\n\n1] Number\n2] Null\n3] Undefined\n4] Boolean\n5] BigInt\n6] Strings\n7] Symbol`,`color:lightgreen;`,`color:white;`);
+
+
+console.log(`\n%cNon Primitive Data Types Has Objects :%c\n\n1] Objects\n2] Array\n3] Dates`,`color:lightgreen;`,`color:white;`);
+
+console.log(`%c\n\n\t\t\t--------- Primitive Data Types ---------\n\n`,`color:yellow;`)
+
+
+let num = 108;
+let num2 = -108;
+let num3 = 108.99;
+
+console.log(`Positive Number -> ${num}\nNegetive Number -> ${num2}\nFolating Point Number -> ${num3}`);
+
+let string = `Java Script`;
+let string2 = `912`;
+let string3 = `NumberWithString AND SYMBOL+-*/!@#$%^&*() = 912`;
+
+
+console.log(`%c---------------------\n`,`color:yellow;`);
+
+console.log(`Charactered String -> ${string}\nNumbered String -> ${string2}\nNumberWithString And Symbols -> ${string3}`);
+
+console.log(`%cString Can Be Any Thing Numbers Symbols And Normal Character Or Mixer Of All`,`color:crimson;`);
+
+
+console.log(`%c---------------------\n`,`color:yellow;`);
+
+let boolean1 = true;
+let boolean2 = false;
+
+console.log(`Boolean Has Only Two Values TRUE And FALSE -> ${boolean1} , ${boolean2}`);
+
+
+
+console.log(`%c---------------------\n`,`color:yellow;`);
+
+
+let bigNumber1 = BigInt(100000008);
+let bigNumber2 = BigInt('100000008');
+let bigNumber3 = 912n;
+
+
+console.log(`BigInt -> ${bigNumber1}\nBigInt -> ${bigNumber2}\nBigInt -> ${bigNumber3}`);
+
+console.log(`\n\nNumber DataTypes Has Limit Of %c- 9007199254740991 TO + 9007199254740991%c If We Want Greater Than This Then We Have To Use BigInt`,`color:lightgreen;`,`color:white;`);
+
+console.log(`%cWhile Using BigInt Keep In Mind You Can Only Put Interger Numbers Not Float Number So Pointed Number Will Give An Error.`,`color:crimson;`);
+
+// let bigNumber4 = BigInt(999999.369);  THIS WILL GIVE AN ERROR
+
+
+
+
